3df44a68365da9e66f1a382e8bee776c823fe48c
David Fisher <fisherds@rose-hulman.edu>
2023-09-28 13:36:44 -0400

    amazing detail page

--- movie_quotes/lib/pages/movie_quote_detail_page.dart
+++ movie_quotes/lib/pages/movie_quote_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_quotes/components/display_card.dart';
 import 'package:movie_quotes/model/movie_quote.dart';
 
 class MovieQuoteDetailPage extends StatefulWidget {
@@ -16,8 +17,28 @@ class MovieQuoteDetailPage extends StatefulWidget {
 class _MovieQuoteDetailPageState extends State<MovieQuoteDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(widget.movieQuote.quote),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Movie Quote"),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        backgroundColor: Colors.black12,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              DisplayCard(
+                title: "Quote:",
+                iconData: Icons.format_quote_outlined,
+                cardText: widget.movieQuote.quote,
+              ),
+              DisplayCard(
+                title: "Movie:",
+                iconData: Icons.movie_filter_outlined,
+                cardText: widget.movieQuote.movie,
+              ),
+            ],
+          ),
+        ));
   }
 }
